8e1f1b447b37b65927ad7e71ee9f00a0efdd5a4d
Pablo Mir <pablo.mir.gomez@gmail.com>
2023-06-06 17:04:58 +0000

    Solución final

--- index.js
+++ index.js
@@ -10,23 +10,61 @@
 const MIN_CELL = 0;
 const MAX_CELL = 255;
 
+const centinelCell = value => {
+    if (value > MAX_CELL) return MIN_CELL;
+    if (value < MIN_CELL) return MAX_CELL;
+    return value;
+}
+
+const getLastIndex = (index, AOS, reverse) => {
+    let loops = 1;
+    if (!reverse) {
+        let i = index + 1;
+        while (i < AOS.length) {
+            if (AOS[i] === '🤜') loops++;
+            if (AOS[i] === '🤛') loops--;
+            if (loops === 0) return i;
+            i++;
+        }
+    } else {
+        let i = index - 1;
+        while (i >= 0) {
+            if (AOS[i] === '🤜') loops--;
+            if (AOS[i] === '🤛') loops++;
+            if (loops === 0) return i;
+            i--;
+        }
+    }
+}
+
 function translate (input) {
     const AOS = Array.from(input);
     let pointer = 0;
     let memory = [0];
-    let indexMemory = 0;
     let output = '';
     for (let i = 0; i < AOS.length; i++) {
         switch(AOS[i]) {
-            case '👉': pointer++; break;
-            case '👈': pointer--; break;
-            case '👆': memory[indexMemory] = pointer; indexMemory++; break;
-            case '👇': memory[indexMemory] = pointer; indexMemory--; break;
+            case '👉': pointer++; memory[pointer] ??= 0; break;
+            case '👈': pointer--; memory[pointer] ??= 0; break;
+            case '👆': memory[pointer] = centinelCell(memory[pointer]+1); break;
+            case '👇': memory[pointer] = centinelCell(memory[pointer]-1); break;
+            case '🤜': 
+                if (memory[pointer] === 0) {
+                    i = getLastIndex(i, AOS, false);
+                }
+                break;
+            case '🤛': 
+                if (memory[pointer] !== 0) {
+                    i = getLastIndex(i, AOS, true);
+                }
+                break;
+            case '👊': output += String.fromCharCode(memory[pointer]); break;
         }
-        output += String.fromCharCode(memory[indexMemory-1]);
+        // console.log(`${AOS[i]} ; ${i} ; ${pointer} ; ${output} ; ${memory}`);
     }
     console.log(output);
 }
 
-translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊');
-console.log('DONE');
+//translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊');
+
+translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊');
