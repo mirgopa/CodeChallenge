30817db6b7e3ff2ccfa8e2ddef5b9de1243c3f7f
Pablo Mir <pablo.mir.gomez@gmail.com>
2023-06-09 14:37:49 +0000

    Optimización de código (+2h)

--- index.js
+++ index.js
@@ -10,52 +10,41 @@
 const MIN_CELL = 0;
 const MAX_CELL = 255;
 
-const centinelCell = value => {
-    if (value > MAX_CELL) return MIN_CELL;
-    if (value < MIN_CELL) return MAX_CELL;
-    return value;
-}
-
-const getLastIndex = (index, AOS, reverse) => {
-    let loops = 1;
-    if (!reverse) {
-        let i = index + 1;
-        while (i < AOS.length) {
-            if (AOS[i] === '🤜') loops++;
-            if (AOS[i] === '🤛') loops--;
-            if (loops === 0) return i;
-            i++;
-        }
-    } else {
-        let i = index - 1;
-        while (i >= 0) {
-            if (AOS[i] === '🤜') loops--;
-            if (AOS[i] === '🤛') loops++;
-            if (loops === 0) return i;
-            i--;
+const processLoops = (AOS) => {
+    let stack = [], loopPairs = [];
+    for (let i = 0; i < AOS.length; i++) {
+        if (AOS[i] === '🤜') {
+            stack.push(i);
+        } else if (AOS[i] === '🤛') {
+            let openLoop = stack.pop();
+            loopPairs[openLoop] = i;
+            loopPairs[i] = openLoop;
         }
     }
+    return loopPairs;
 }
 
-function translate (input) {
+const translate = (input) => {
     const AOS = Array.from(input);
+    let loopPairs = processLoops(AOS); // Generamos estructura con pares de índices para loops
     let pointer = 0;
     let memory = [0];
     let output = '';
+
     for (let i = 0; i < AOS.length; i++) {
         switch(AOS[i]) {
             case '👉': pointer++; memory[pointer] ??= 0; break;
             case '👈': pointer--; memory[pointer] ??= 0; break;
-            case '👆': memory[pointer] = centinelCell(memory[pointer]+1); break;
-            case '👇': memory[pointer] = centinelCell(memory[pointer]-1); break;
+            case '👆': memory[pointer] = memory[pointer]+1 > MAX_CELL ? MIN_CELL : memory[pointer]+1; break;
+            case '👇': memory[pointer] = memory[pointer]-1 < MIN_CELL ? MAX_CELL : memory[pointer]-1; break;
             case '🤜': 
                 if (memory[pointer] === 0) {
-                    i = getLastIndex(i, AOS, false);
+                    i = loopPairs[i];
                 }
                 break;
             case '🤛': 
                 if (memory[pointer] !== 0) {
-                    i = getLastIndex(i, AOS, true);
+                    i = loopPairs[i];
                 }
                 break;
             case '👊': output += String.fromCharCode(memory[pointer]); break;
@@ -65,6 +54,6 @@ function translate (input) {
     console.log(output);
 }
 
-//translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊');
+translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊');
 
 translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊');
